863eb62aa59d4d9f52576871204f988eba175cad
Jonathan Funk <johnthomasfunk@gmail.com>
2020-09-05 00:02:26 -0700

    Implement main program driver

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
-package beasty
+package main
 
+import (
+	"os"
+)
+
+// main driver for program
+// initializes bot and sets to listen for guild events
 func main() {
-	b := NewBeasty("", "")
+	var token string
+	// if cli arguments passed, assume ENV override
+	if len(os.Args) != 1 {
+		arguments := os.Args[1:]
+		token = arguments[0]
+	} else {
+		token = os.Getenv("TOKEN")
+	}
+	b := NewBeasty(token)
 	b.Start()
 }
