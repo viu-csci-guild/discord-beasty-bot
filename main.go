b4a83545240d85d1ed493e6693fda3c8d682663d
Jonathan Funk <johnthomasfunk@gmail.com>
2020-09-07 10:11:05 -0700

    Add local usage set in main driver for testing

--- main.go
+++ main.go
@@ -8,13 +8,17 @@ import (
 // initializes bot and sets to listen for guild events
 func main() {
 	var token string
+	var localUse bool
+
 	// if cli arguments passed, assume ENV override
 	if len(os.Args) != 1 {
+		localUse = true
 		arguments := os.Args[1:]
 		token = arguments[0]
 	} else {
 		token = os.Getenv("TOKEN")
 	}
 	b := NewBeasty(token)
+	b.SetLocalUse(localUse)
 	b.Start()
 }
