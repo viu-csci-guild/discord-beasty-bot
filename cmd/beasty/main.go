2e26fe45d79d2ca9dfcede0394c9052675065db0
Jonathan Funk <johnthomasfunk@gmail.com>
2021-08-28 20:22:19 -0700

    Remove unused watchroom var

--- cmd/beasty/main.go
+++ cmd/beasty/main.go
@@ -11,10 +11,9 @@ func main() {
 	// if cli arguments passed, assume ENV override
 	token := os.Getenv("TOKEN")
 	studentRoleID := os.Getenv("STUDENT_ROLE_ID")
-	watchRoomID := os.Getenv("WATCH_ROOM_ID")
 	startupRoomID := os.Getenv("START_ROOM_ID")
 	serverID := os.Getenv("SERVER_ID")
 
-	b := client.NewBeasty(token, studentRoleID, watchRoomID, startupRoomID, serverID)
+	b := client.NewBeasty(token, studentRoleID, startupRoomID, serverID)
 	b.Start()
 }
